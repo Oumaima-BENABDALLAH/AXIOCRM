e243ddbff943541d320238a9c3d173582d9f72dd
Oumaima-BENABDALLAH <oumaimabenabdallah26@gmail.com>
2026-02-21 12:28:42 +0100

    fix erreurs merge

--- Backend/Models/Order.cs
+++ Backend/Models/Order.cs
@@ -15,7 +15,7 @@ namespace ProductManager.API.Models
       
         public decimal? CashAmount { get; set; }
         public DateTime? PaymentDate { get; set; }
-        public int? DeliveryMethodId { get; set; 
+        public int? DeliveryMethodId { get; set; } 
         public string? CardNumber { get; set; }
         public string? CardHolder { get; set; }
         public string? ExpiryDate { get; set; }
