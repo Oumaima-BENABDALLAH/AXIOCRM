728631f80a5c667e292137b3972ad754afc18ded
Oumaima-BENABDALLAH <oumaimabenabdallah26@gmail.com>
2026-02-21 12:37:44 +0100

    some fix

--- Backend/Models/Order.cs
+++ Backend/Models/Order.cs
@@ -5,7 +5,6 @@ namespace ProductManager.API.Models
     public class Order
     {
         public int Id { get; set; }
-
         public int ClientId { get; set; }
         public Client? Client { get; set; } 
         public DateTime OrderDate { get; set; }
